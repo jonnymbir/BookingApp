db3210c697bba87ff5cdbb4d040173cd600b731e
John Mbir <jonnymbir@gmail.com>
2022-07-17 22:40:58 +0000

    hotel routes completed

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,28 @@
 import express from "express";
+import {
+  getHotels,
+  getHotel,
+  createHotel,
+  deleteHotel,
+  updateHotel,
+} from "../controllers/hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+//CREATE RECORD
+router.post("/", verifyAdmin, createHotel);
+
+//UPDATE RECORD
+router.put("/:id", verifyAdmin, updateHotel);
+
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+
+//GET
+router.get("/:id", getHotel);
+
+//GET ALL
+router.get("/", getHotels);
 
 export default router;
